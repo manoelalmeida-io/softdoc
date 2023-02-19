18ca06b7b87cae1e4afe8d6c6bea8cea76228891
Manoel Almeida <manoel.almeida@sptech.school>
2023-02-18 21:10:28 -0300

    Adding checkstyle configuration file

--- document-service/src/main/java/school/sptech/documentservice/DocumentServiceApplication.java
+++ document-service/src/main/java/school/sptech/documentservice/DocumentServiceApplication.java
@@ -3,11 +3,15 @@ package school.sptech.documentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Document service starter class.
+ *
+ * @author Manoel Almeida
+ */
 @SpringBootApplication
 public class DocumentServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DocumentServiceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(DocumentServiceApplication.class, args);
+  }
 }
